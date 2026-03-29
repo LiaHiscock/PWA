2d6ceed88b40d40b5c4e61232cdb73a10ef02892
Lia Hiscock <liahiscock@outlook.com>
2026-03-29 00:05:06 -0700

    increase line spacing

--- SongAdder/sw.js
+++ SongAdder/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'song-adder-v2';
+const CACHE_NAME = 'song-adder-v3';
 const ASSETS = [
     './',
     './index.html',
@@ -8,6 +8,7 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', event => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
     );
@@ -23,6 +24,10 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(cached => cached || fetch(event.request))
+        fetch(event.request).then(response => {
+            const clone = response.clone();
+            caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+            return response;
+        }).catch(() => caches.match(event.request))
     );
 });
