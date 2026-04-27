e511f6ad00e873971ff5a1a9edf0009e9699f78e
Lia Hiscock <liahiscock@outlook.com>
2026-04-27 16:58:00 -0700

    update manifest install test page

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const navigatorButton = document.getElementById("navigatorButton");
 navigatorButton.addEventListener("click", async() => {
   try {
     let install_url = "https://kbhlee2121.github.io/pwa/web-install/manifest.webmanifest";
-    await navigator.install(install_url).then((result) => {
+    await navigator.installFromManifest(install_url).then((result) => {
       console.log(result);
     });
   } catch (err) {
